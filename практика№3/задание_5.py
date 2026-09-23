f2a99387ba6f2bd7f9c75b9e54688f108080c2df
Vladimir vitushenkov <vovadiscord1@gmail.com>
2026-09-23 19:41:21 +0300

    Add files via upload

---
+++ практика№3/задание_5.py
@@ -0,0 +1,9 @@
+shopping_list = ['"молоко", "соль", "сахар", "перец"']
+
+shopping_list.append("шоколад")
+
+print(shopping_list)
+
+unique_items = set(shopping_list) #при выводе множества меняются скобки с квадратных на фигурные
+
+print(unique_items)
